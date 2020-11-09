6e6aacfec519db375a26d4064f301cce8568d6ba
Aleksandra Górska <56221306+Nim98@users.noreply.github.com>
2020-11-09 13:01:47 +0100

    SQL connection

--- Serwer/Serwer/src/serwer/Serwer.java
+++ Serwer/Serwer/src/serwer/Serwer.java
@@ -4,6 +4,11 @@ package serwer;
 import java.io.*;
 import java.util.*;
 import java.net.*;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Serwer {
 
@@ -49,10 +54,20 @@ class ClientHandler implements Runnable {
     @Override
     public void run() {
         String received;
-        
+        List data = new ArrayList<>();
+        data.add("20625071");
+        data.add(1);
+        data.add(2);
+        data.add(3);
+        data.add(4);
+        data.add(5);
+        data.add(6);
+        data.add(7.4);
         while (true) {
             try {
-                received = inputData.readUTF();
+                //odbior wiadomoœci
+            	connectDB(true, data);
+            	received = inputData.readUTF();
                 System.out.println("["+this.memberName+"] "+received);
                 String Message= received;
                 if (received.equals("logout")) {
@@ -88,6 +103,62 @@ class ClientHandler implements Runnable {
             catch (IOException e) {
                 e.printStackTrace();
             }
-
     }
+            
+    public void connectDB(Boolean isGet, List data) {
+    	List results = new ArrayList();
+    	Boolean inDB = false;
+    	try{
+    		Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/barcode_data","root", "");
+            Statement statement = connection.createStatement();
+            System.out.print("Database is connected !");
+            String sql_check = "SELECT * FROM barcode WHERE code = "+data.get(0)+";";
+            ResultSet check = statement.executeQuery(sql_check);
+            if(check.next()) {
+            	if (check.getString("code").length()==0) {
+            		inDB = false;
+            	}
+            	else {
+            		inDB = true;
+            	}
+            }
+            System.out.print(inDB);;
+            if(isGet) {
+            	if(inDB) {
+	            	String sql_select = "SELECT * FROM barcode WHERE code = "+data.get(0)+";";
+	            	ResultSet result = statement.executeQuery(sql_select);
+	            	if (result.next()) {
+		            	results.add(result.getString("code"));
+		            	results.add(result.getString("calorie"));
+		            	results.add(result.getString("fat"));
+		            	results.add(result.getString("saturated"));
+		            	results.add(result.getString("carb"));
+		            	results.add(result.getString("sugar"));
+		            	results.add(result.getString("protein"));
+		            	results.add(result.getString("sodium"));
+		            	System.out.print(results);
+	            	}
+            	}
+            	else {
+            		System.out.println("No data");
+            	}
+            }	
+            else {
+            	if(inDB) {
+            		System.out.println("Already exists");
+            	}
+            	else {
+	            	String sql_insert = "INSERT INTO barcode(code, calorie, fat, saturated, carb, sugar, protein, sodium) VALUES ("+data.get(0)+","+data.get(1)+","+data.get(2)+","+data.get(3)+","+data.get(4)+","+data.get(5)+","+data.get(6)+","+data.get(7)+");";
+	            	statement.executeUpdate(sql_insert);
+            	}
+            }
+            statement.close();
+            connection.close();
+         }
+         catch(Exception e) {
+            System.out.print("Do not connect to DB - Error:"+e);
+         }
+    }
+    
 }
