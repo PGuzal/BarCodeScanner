fdb3e4a341e74f7e9174a110c7a890bbf4e8cbfa
Aleksandra Górska <56221306+Nim98@users.noreply.github.com>
2020-11-13 12:57:28 +0100

    Cleaning

--- app/src/main/java/com/example/barcodescanner/MainActivity.kt
+++ app/src/main/java/com/example/barcodescanner/MainActivity.kt
@@ -5,15 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
-import com.google.gson.Gson
 import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_result.*
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody.Companion.toRequestBody
-import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
